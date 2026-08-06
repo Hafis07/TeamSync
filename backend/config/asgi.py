4988b9d7822a12e5e5209549defd6c6545880a5b
hafis993 <muhdhfs993@gmain.com>
2026-08-06 12:53:34 +0530

    Fix ASGI import order

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -3,10 +3,12 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 from django.core.asgi import get_asgi_application
+
+# Load Django first
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
-
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
     {
