d940fd1e8ba2b41e41a1942bce61f08ca821ac42
hafis993 <muhdhfs993@gmain.com>
2026-08-06 12:24:09 +0530

    Fix ASGI settings initialization

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,11 +1,10 @@
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
-import chat.routing
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
 
 django_asgi_app = get_asgi_application()
 
